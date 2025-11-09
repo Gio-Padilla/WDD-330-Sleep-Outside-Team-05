c2406c63c60cda8faa69cca312c652d3eb251fef
Gio-Padilla <pad24005@byui.edu>
2025-11-08 21:51:32 -0600

    I worked through the full "W02 Team Activity: Dynamic Product Detail Page" I did everything that was asked. All of the product details pages run dynamically. Per the asignment, I moved some things arround and added things. I also added/fixed the issue with the image disapearing.

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,15 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData("tents");
+const productId = getParam('product');
 
-function addProductToCart(product) {
-    const cart = getLocalStorage("so-cart");
-    // Normalize stored cart so we always work with an array.
-    const cartItems = Array.isArray(cart) ? cart : cart ? [cart] : [];
-    cartItems.push(product);
-    setLocalStorage("so-cart", cartItems);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+// Moved addProductToCart to ProductDetails.mjs as asignment stated
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
