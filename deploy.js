117547392e4b6e733a33ece9e9dda29c51de7a5e
Gio-Padilla <pad24005@byui.edu>
2025-11-20 19:49:03 -0600

    Should work

--- deploy.js
+++ deploy.js
@@ -1,12 +1,14 @@
 const ghpages = require('gh-pages');
+const path = require('path');
 
 ghpages.publish(
-  'dist',
+  path.join(__dirname, 'dist'),
   {
-    branch: 'gh-pages',             // target branch
-    dotfiles: true,                  // include .nojekyll etc.
-    push: true,                      // force push
+    branch: 'gh-pages',         // target branch
+    dotfiles: true,              // include .nojekyll etc.
+    push: true,                  // force push
     message: 'Deploying latest build', 
+    repo: 'https://github.com/Gio-Padilla/WDD-330-Sleep-Outside-Team-05.git',
   },
   (err) => {
     if (err) console.error('Deployment failed:', err);
